988d09cd3ed6541f32a31cc920faec02c15486fe
martinsik <m.sikora@make.com>
2022-08-09 10:49:01 +0200

    initial commit

---
+++ apps/myapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-nx14-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'myapp';
+}
